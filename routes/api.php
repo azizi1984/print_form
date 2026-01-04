0502ba204d4ff4a9d1989ec5f0b6898886172835
azizi1984 <chi.laisema@gmail.com>
2026-01-05 05:14:13 +0700

    update api web route

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,37 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Http\Controllers\Api\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-Route::get('/test', function (Request $request) {
-    return true;
+Route::post('application/login', [AuthController::class, 'login']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    //auth
+    Route::post('application/refreshToken', [AuthController::class, 'refreshToken']);
+    Route::post('application/logout', [AuthController::class, 'logout']);
+    
+    Route::get('application/test', function (Request $request) {
+        return "API Test";
+    });
+
+    Route::get('application/generateToken', function () {
+    $token = Str::random(125);
+        return [
+            'token' => $token,
+            'hash' => Hash::make($token),
+        ];
+    });
+});
+
+Route::post('application/freeToken', function (Request $request) {
+    $token = $request->input('value');
+    return [
+        'token' => $token,
+        'hash' => Hash::make($token),
+    ];
 });
