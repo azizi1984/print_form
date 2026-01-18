5c5942bf419a0115610b316ad858cebf1080299e
azizi1984 <chi.laisema@gmail.com>
2026-01-19 04:23:41 +0700

    update route api

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ProfileController;
+use App\Http\Controllers\Api\UserController;
 
 Route::post('application/login', [AuthController::class, 'login']);
 
@@ -17,9 +18,17 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('application/logout', [AuthController::class, 'logout']);
 
     //profile
-    Route::post('application/profile/store', [ProfileController::class, 'store']);
+    Route::post('application/profile/create', [ProfileController::class, 'store']);
     Route::put('application/profile/update', [ProfileController::class, 'update']);
     Route::delete('application/profile/delete', [ProfileController::class, 'destroy']);
+    Route::get('application/profile/getAllProfiles', [ProfileController::class, 'getAllProfiles']);
+    
+    //user
+    Route::post('application/user/create', [UserController::class, 'store']);
+    Route::put('application/user/update', [UserController::class, 'update']);
+    Route::delete('application/user/delete', [UserController::class, 'destroy']);
+    Route::get('application/user/getAllUsers', [UserController::class, 'getAllUsers']);
+    
     
     
     Route::get('application/test', function (Request $request) {
